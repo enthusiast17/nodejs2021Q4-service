bdafa4f77fea12bfaaf89fc17cb46bd8cdeafe51
Ulan Nurym <ulan17nurym@gmail.com>
2022-02-05 17:54:00 +0600

    refactor: remove express response from user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,17 +3,15 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
-  Res,
   HttpStatus,
   Put,
+  HttpCode,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Response } from 'express';
 import { toUserResponse } from 'src/common/responses';
 
 @Controller('users')
@@ -21,41 +19,28 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
-    return res
-      .status(HttpStatus.CREATED)
-      .json(toUserResponse(await this.userService.create(createUserDto)));
+  async create(@Body() createUserDto: CreateUserDto) {
+    return toUserResponse(await this.userService.create(createUserDto));
   }
 
   @Get()
-  async findAll(@Res() res: Response) {
-    return res
-      .status(HttpStatus.OK)
-      .json((await this.userService.findAll()).map(toUserResponse));
+  async findAl() {
+    return (await this.userService.findAll()).map(toUserResponse);
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string, @Res() res: Response) {
-    return res
-      .status(HttpStatus.OK)
-      .json(toUserResponse(await this.userService.findById(id)));
+  async findOne(@Param('id') id: string) {
+    return toUserResponse(await this.userService.findById(id));
   }
 
   @Put(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() updateUserDto: UpdateUserDto,
-    @Res() res: Response,
-  ) {
-    return res
-      .status(HttpStatus.OK)
-      .json(toUserResponse(await this.userService.update(id, updateUserDto)));
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return toUserResponse(await this.userService.update(id, updateUserDto));
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string, @Res() res: Response) {
-    return res
-      .status(HttpStatus.NO_CONTENT)
-      .json(await this.userService.remove(id));
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id') id: string) {
+    return await this.userService.remove(id);
   }
 }
